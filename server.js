56ca2527c33a38bc989a7f21bd7225ef5a433980
WD-201_Andrei_Bagang <98503728+Wontat@users.noreply.github.com>
2022-03-02 22:45:42 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 //Bagang, Andrei C.
 //WD-201
-//GET-METHOD
+//POST-METHOD
 
 var express = require('express'); 
 var app = express();
+var bodyParser = require('body-parser');
+
+//Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({extended:false})
 
 app.use(express.static('public'));
 app.get('/index.html', function (req, res) {
   res.sendFile(__dirname + "/" + "index.html"); 
 })
 
-app.get('/process_get', function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
   // Prepare output in JSON format
   response = {
-    first_name: req.query.first_name, 
-    last_name: req.query.last_name,
-    stud_course: req.query.stud_course,
-    stud_age: req.query.stud_age,
-    stud_comments: req.query.stud_comments
+    first_name:req.body.first_name, 
+    last_name:req.body.last_name,
+    stud_course:req.body.stud_course,
+    stud_age:req.body.stud_age,
+    stud_comments:req.body.stud_comments
   };
   
   console.log(response);
   res.end(JSON.stringify(response));
 })
 
-var server = app.listen(5000, function () {
+var server = app.listen(1206, function () {
   var host = server.address().address 
   var port = server.address().port
 
